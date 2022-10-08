640f2ef3b496091206808241121b360df3d411c9
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-07 20:49:32 -0600

    + feat: add infinite scroll to load posts

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostsService } from '@fotosgram/services';
 import { Post } from '@fotosgram/types';
 
@@ -7,12 +7,26 @@ import { Post } from '@fotosgram/types';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   posts: Post[] = [];
 
-  constructor(private postsService: PostsService) {
-    this.postsService.getPosts().subscribe(({ posts }) => (this.posts = posts));
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.loadPosts();
   }
 
   onClick() {}
+
+  loadPosts(event: any = null) {
+    this.postsService.getPosts().subscribe(({ posts, results }) => {
+      this.posts.push(...posts);
+      if (event) {
+        event.target.complete();
+      }
+      if (results === 0) {
+        event.target.disabled = true;
+      }
+    });
+  }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,18 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PostResponse } from '@fotosgram/types';
 import { environment } from 'src/environments/environment';
-
+import { filter } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
+  private page = 0;
   constructor(private http: HttpClient) {}
 
-  getPosts(page = 1) {
+  getPosts() {
+    this.page++;
     return this.http.get<PostResponse>(`${environment.apiUrl}/posts`, {
       params: {
-        page,
-      }
+        page: this.page,
+      },
     });
   }
 }
