8ccdb8eb290bd9d484700fa97aadb8a769df2292
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-07 18:01:59 -0600

    + add responsive behavior to posts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [PostsComponent, PostComponent],
   exports: [PostsComponent, PostComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
 })
 export class ComponentsModule {}
