039dbecba0e5c7621d16c2df52b26fefaf52ebaf
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-07 20:15:36 -0600

    + feat: add domSanitizer pipe

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
 import { IonicModule } from '@ionic/angular';
+import { PipesModule } from '@fotosgram/pipes';
 
 @NgModule({
   declarations: [PostsComponent, PostComponent],
   exports: [PostsComponent, PostComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, PipesModule],
 })
 export class ComponentsModule {}
